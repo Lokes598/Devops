1fc8c1b98666407aa5576e439cc2e6d9aaa9f2a5
root <root@ip-172-31-42-221.eu-north-1.compute.internal>
2024-08-13 10:13:20 +0000

    git_file.sh

---
+++ git_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# version 1
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for number in range(1, 101):
+    if is_prime(number):
+        print(number)
